2d3fa140fddeb410d8397d197296ac67ff2f178f
Коворкинг 10 <marsel.ahmetov974@gmail.com>
2023-04-08 14:12:40 +0300

    added age checker

--- bot/marselApi.py
+++ bot/marselApi.py
@@ -1,13 +1,32 @@
 import requests
+import random
+
+
+class User:
+    def __init__(self, name, gender, age):
+        self.name = name
+        self.gender = gender
+        self.age = age
+
+    def check_age(self, anime):
+        anime_age_rating = anime["data"]["attributes"]["ageRating"]
+        print(anime_age_rating)
+
+
 # https://kitsu.io/api/edge/anime?filter[text]=tokio
 
-link = "http://numbersapi.com/"  # get-запрос на определенный api
-link2 = "https://catfact.ninja/facta"  # get-запрос на определенный api
-response = requests.get(link2)  # отправка нашего запроса и запись в response
-if response.ok:
-    data = response.json()
-    print(response)
-    print(data['fact'])
-    print(data['length'])
-else:
-    print("Ошибка запроса " + response.text)
+def random_anime():
+    maximum = 13000
+    rand = random.randint(1, maximum)
+    link = "https://kitsu.io/api/edge/anime/"  # get-запрос на определенный api
+    link += str(rand)
+    response = requests.get(link)
+    if response.ok:
+        return response.json()
+    else:
+        return random_anime()
+
+
+anime = random_anime()
+nikita = User("Никита", "male", 12)
+nikita.check_age(anime)
